7a6b822ebe478c7f38c16f93829126f2c0a046b0
Ali Neaz <neazali77@gmail.com>
2021-07-28 20:25:54 +0600

    completed if elif else ladder also using boolean and

---
+++ 01_conditionals.py
@@ -0,0 +1,38 @@
+
+# a = int(input("Enter a number: \n"))
+
+# ********if elif else ladder in python ********
+# if(a < 3):
+#     print("A is greater than  to 3")  # indent = 4 spaces or 1 tab
+# elif(a > 7):
+#     print("A is greater than  to 7")
+# elif(a > 15):
+#     print("A is greater than  to 15")
+# elif(a > 25):
+#     print("A is greater than  to 25")
+# else:
+#     print("A is not greater than 3 or 7")
+# print("Done")
+
+
+# *******************************multiple if else statements*******************************
+# a = int(input("Enter a number: \n"))  # redefining a
+
+# if(a > 3):
+#     print("a is greater than 3")
+# if(a > 7):
+#     print("a is greater than seven")
+# if(a > 25):
+#     print("a is greater than 25")
+# elif(a > 30):
+#     print("a is greater than 30")
+# else:
+#     print("a is smaller than all")
+# print("Done")
+
+
+b = 45
+if(b > 9):
+    print(b, "is greater than 9")
+else:
+    print(b, "is not greater than 9")
---
+++ 02_prob_01.py
@@ -0,0 +1,6 @@
+# if age is greater than 18 print yes or print no
+a = int(input("Enter your age: \n"))
+if(a >= 18 and a <= 60):
+    print("yes you can watch it at", a)
+else:
+    print("No! you can't watch it at", a)
