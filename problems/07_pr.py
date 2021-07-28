596788af6ae6532c24969f3ac66c241df4768486
Ali Neaz <neazali77@gmail.com>
2021-07-29 01:40:59 +0600

    added the 7th problem

---
+++ problems/07_pr.py
@@ -0,0 +1,7 @@
+# post = "Once upon a time there was a boy named Ali Neaz. He was very stupid and still he is a stupid"
+post = input("Enter the post: \n")
+
+if "Ali Neaz" in post or "ali neaz" in post:
+    print("they are talking about Ali Neaz")
+else:
+    print("they are not talking about Ali Neaz")
